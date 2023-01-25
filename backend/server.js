61c0d4363a090535bf00d549c8133921c1b2b45c
Shehryar Hassan <phonetech195@gmail.com>
2023-01-26 00:57:12 +0100

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,14 @@ const app = express();
 // Use helmet for security headers
 app.use(cors());
 app.use(helmet());
+
+// Connect to MongoDB
+require("dotenv").config();
+mongoose.set("strictQuery", false); // allows us to use the find() method without passing in an object
+const uri = process.env.ATLAS_URI; // uri is the path to our MongoDB database
+mongoose.connect(uri);
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
