f8cef7e3623bc76f2dea67ae26b613b08ad324ae
Shehryar Hassan <phonetech195@gmail.com>
2023-01-26 01:03:10 +0100

    userSchema, bcrypt

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash the user's password before saving
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model("User", userSchema);
