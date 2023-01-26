cdcb71bc872caf461f00b671e536b53d08b78e48
Shehryar Hassan <phonetech195@gmail.com>
2023-01-26 01:03:32 +0100

    route

---
+++ backend/routes/products.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+let Product = require("../models/product.model");
+
+router.route("/").get((req, res) => {
+  Product.find()
+    .then((products) => res.json(products))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
