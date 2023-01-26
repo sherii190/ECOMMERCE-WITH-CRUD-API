7c78cd18b61c6cdfb58cd5e8e51c90c92cf81d6b
Shehryar Hassan <phonetech195@gmail.com>
2023-01-26 01:05:36 +0100

    user, login

---
+++ backend/routes/users.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+let User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const newUser = new User({
+    email,
+    password,
+  });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
