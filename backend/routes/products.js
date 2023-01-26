967b8cb8164bcd4b4d5f9670daf07f83eeebd6d3
Shehryar Hassan <phonetech195@gmail.com>
2023-01-26 01:03:56 +0100

    /add

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,3 +6,26 @@ router.route("/").get((req, res) => {
     .then((products) => res.json(products))
     .catch((err) => res.status(400).json("Error: " + err));
 });
+
+router.route("/add").post((req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = Number(req.body.price);
+  const stock = Number(req.body.stock);
+  const image = req.body.image;
+  const category = req.body.category;
+
+  const newProduct = new Product({
+    title,
+    description,
+    price,
+    stock,
+    image,
+    category,
+  });
+
+  newProduct
+    .save()
+    .then(() => res.json("Product added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
